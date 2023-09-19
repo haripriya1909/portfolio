74e79ccd6ee6d6469dcdad52409e2b194d3a6733
hari1909 <hari19september@gmail.com>
2023-09-19 13:42:55 +0530

    homwpage and about

--- docs/script.js
+++ docs/script.js
@@ -3,6 +3,7 @@ function toggleMenu()
     var x = document.getElementsByClassName("hamburger");
     if(x.style.display == "flex"){
         x.style.display = "none";
+        
     }
     else{
         x.style.display = "block";
